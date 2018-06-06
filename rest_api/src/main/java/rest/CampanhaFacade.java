75db42a0ca9cb73b3d73e111465756415d0b93ca
Manuel Felizardo <manuel.felizardo@ua.pt>
2018-06-06 22:50:54 +0100

    New tests

--- rest_api/src/main/java/rest/CampanhaFacade.java
+++ rest_api/src/main/java/rest/CampanhaFacade.java
@@ -8,17 +8,13 @@ package rest;
 import JPA.Campanha;
 import JPA.CampanhaCreate;
 import JPA.Utilizador;
-import java.util.ArrayList;
 import java.util.List;
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -29,63 +25,52 @@ import javax.ws.rs.core.MediaType;
 
 /**
  *
- * @author Dimitri
+ * @author root
  */
 @Stateless
-@Path("campaign")
-public class CampanhaFacadeREST{
+public class CampanhaFacade extends AbstractFacade<Campanha> {
 
-    @EJB
-    CampanhaFacade facade;
-    
+    @PersistenceContext(unitName = "PERSISTENCE_UNIT_NAME")
+    private EntityManager em;
 
-    public CampanhaFacadeREST() {
+    public CampanhaFacade() {
+        super(Campanha.class);
     }
 
-    @POST
-    @Consumes({ MediaType.APPLICATION_JSON})
     public Long create2(CampanhaCreate campanha) {
-        return facade.create2(campanha);
+        Utilizador u = em.find(Utilizador.class, campanha.user_id);
+        Campanha c = new Campanha(campanha.title, campanha.description, campanha.goal, u,campanha.image);
+        super.create(c);
+        return c.getId();
     }
 
-    @PUT
-    @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public void edit(@PathParam("id") Long id, Campanha entity) {
-        facade.edit(id, entity);
+        super.edit(entity);
     }
 
-    @DELETE
-    @Path("{id}")
     public void remove(@PathParam("id") Long id) {
-        facade.remove(id);
+        super.remove(super.find(id));
     }
 
-    @GET
-    @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public Campanha find(@PathParam("id") Long id) {
-        return facade.find(id);
+        return super.find(id);
     }
 
-    @GET
-    @Produces({ MediaType.APPLICATION_JSON})
     public List<Campanha> findAll() {
-        return facade.findAll();
+        TypedQuery query = em.createQuery("SELECT p FROM Campanha p", Campanha.class);
+        return query.getResultList();
     }
 
-    @GET
-    @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public List<Campanha> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
-        return facade.findRange(from, to);
+        return super.findRange(new int[]{from, to});
     }
 
-    @GET
-    @Path("count")
-    @Produces(MediaType.TEXT_PLAIN)
     public String countREST() {
-        return facade.countREST();
+        return String.valueOf(super.count());
     }
 
+    @Override
+    protected EntityManager getEntityManager() {
+return em;
+    }
 }
